156c908f617da84b31e67eece9dd1a71cb1165d7
Gleb <prasionissy@gmail.com>
2025-04-09 14:00:14 +0500

    minor changed

--- entry_control/app.py
+++ entry_control/app.py
@@ -1,20 +1,20 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from contextlib import asynccontextmanager
 from models.target.base import Base as TargetBase
 from normalizer import Normalizer
-from config import SQLITE_PATH, POSTGRES_CONFIG
 from db.db_pg import engine as pg_engine, get_db as pg_get_db
 from db.db_sqlite import get_db as sqlite_get_db
 from models import tables_list as target_tables_list
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    TargetBase.metadata.drop_all(pg_engine)
+def init_target_db():
+    TargetBase.metadata.drop_all(pg_engine) 
     TargetBase.metadata.create_all(bind=pg_engine, tables=target_tables_list)
-    yield
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    init_target_db()
 
 @app.post("/normalize")
 def normalize(
@@ -27,3 +27,7 @@ def normalize(
         return {"status": "success"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Normalization failed: {e}")
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
